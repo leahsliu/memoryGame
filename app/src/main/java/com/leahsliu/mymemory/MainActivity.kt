15c24166b82564306bce1e0594e900a9b275d331
Leah Liu <leahliu@Leahs-MacBook-Pro.local>
2024-02-09 17:39:01 -0800

    Created card view - disabled overscroll

--- app/src/main/java/com/leahsliu/mymemory/MainActivity.kt
+++ app/src/main/java/com/leahsliu/mymemory/MainActivity.kt
@@ -3,6 +3,7 @@ package com.leahsliu.mymemory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +18,12 @@ class MainActivity : AppCompatActivity() {
         rvBoard = findViewById(R.id.rvBoard)
         tvNumMoves = findViewById(R.id.tvNumMoves)
         tvNumPairs = findViewById(R.id.tvNumPairs)
+
+        // provide a binding for the data set to the views of the RecyclerView
+        rvBoard.adapter = MemoryBoardAdapter(this, 8)
+        rvBoard.setHasFixedSize(true)
+        // measures and positions item views
+        rvBoard.layoutManager = GridLayoutManager(this,2)
     }
 
 }
